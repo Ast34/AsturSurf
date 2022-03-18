8a404d23bd8cf42c21ebc3a54a0d29b1ce732344
AndresCabrero <101799418+AndresCabrero@users.noreply.github.com>
2022-03-18 08:51:59 +0100

    LetraUsuario

--- scripts/header.js
+++ scripts/header.js
@@ -2,6 +2,7 @@ var nombreu = document.getElementById("nombreusuario");
 function Nombre(){
   var usuario = sessionStorage.getItem("nombreusuario").charAt(0).toUpperCase();
   if(usuario != undefined){
+    nombreu.style.display="block";
     nombreu.innerHTML = usuario;
   }else{
     usuario = "none"
